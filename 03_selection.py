57dd5cb57c214b3808cd6aac554aca37fb957213
Cristy Knijff <cristy.knijff@gmail.com>
2026-06-30 22:14:25 +0200

    Update 03_selection.py

--- 03_selection.py
+++ 03_selection.py
@@ -1,1 +1,14 @@
+try:
+    experience = int(input("How many years of work experience do you have? "))
 
+    if experience < 0:
+        print("Invalid input.")
+    elif experience <= 2:
+        print("You are a junior.")
+    elif experience <= 5:
+        print("You are a mid-level professional.")
+    else:
+        print("You are a senior.")
+
+except ValueError:
+    print("Invalid input. Please enter a number.")
