5c95ed025a283131babefb224a6740ed72d7a838
Cristy Knijff <cristy.knijff@gmail.com>
2026-06-30 22:12:54 +0200

    Update 01_sequential.py

--- 01_sequential.py
+++ 01_sequential.py
@@ -1,1 +1,17 @@
+print("Welcome to my profile! Here I will demonstrate a sequential algorithm.")
 
+name = "Cristy"
+
+work_experience1 = "Business Development Representative (in sales)"
+work_experience2 = "Electrical Design Engineer & Work Planner (Medium and Low Voltage)"
+work_experience3 = "Cost Engineer Measurement & Control Technology (Building Automation)"
+work_experience4 = "Design Engineer Building Management Systems (Software & Hardware)"
+
+print("Name:", name)
+print("Work Experience:")
+print(work_experience1)
+print(work_experience2)
+print(work_experience3)
+print(work_experience4)
+
+print("Thank you for your attention.")
