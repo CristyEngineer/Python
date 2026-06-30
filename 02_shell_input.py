5a80b79f3d642aba1c61504676f2db594505fd58
Cristy Knijff <cristy.knijff@gmail.com>
2026-06-30 22:13:51 +0200

    Update 02_shell_input.py

--- 02_shell_input.py
+++ 02_shell_input.py
@@ -1,1 +1,10 @@
+print("Shell input program")
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+full_name = f"{first_name} {last_name}"
+
+print(f"\nHello {full_name}, nice to meet you!")
+print("Welcome to my GitHub profile program 🚀")
 
