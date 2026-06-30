315053e873aaf820f26ecfc07f44044c433be7fd
Cristy Knijff <cristy.knijff@gmail.com>
2026-06-30 22:15:31 +0200

    Update 05.boolean_expressions.py

--- 05.boolean_expressions.py
+++ 05.boolean_expressions.py
@@ -1,1 +1,13 @@
+print("This program checks whether a number is even or odd.")
+try:
+    x = int(input("Please enter a number: "))
+    is_even = (x % 2 == 0)
+
+    if is_even:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+except ValueError:
+    print("Invalid input. Please enter an integer number.")
 
