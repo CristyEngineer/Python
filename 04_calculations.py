fd8b9641660f2704f2f6b9405471c5478e6dc3aa
Cristy Knijff <cristy.knijff@gmail.com>
2026-06-30 22:15:02 +0200

    Update 04_calculations.py

--- 04_calculations.py
+++ 04_calculations.py
@@ -1,1 +1,13 @@
-
+print("Please enter two numbers and use a dot (.) as the decimal separator.")
+try: 
+  a = float(input("Number A: ")) 
+  b = float(input("Number B: ")) 
+  print("The sum of A and B is:", a + b) 
+  print("The difference between A and B is:", a - b) 
+  print("The product of A and B is:", a * b) 
+  if b != 0: 
+    print("Division:", a / b) 
+  else: 
+    print("Division: Cannot divide by zero") 
+except ValueError: 
+  print("Invalid input. Please enter a number.")
